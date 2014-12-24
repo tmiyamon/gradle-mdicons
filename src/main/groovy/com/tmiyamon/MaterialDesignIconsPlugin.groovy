7875a5db3b66facce5c5797666e9c6beb4fe9344
tmiyamon <miyamototakuya@gmail.com>
2014-12-25 01:20:15 +0900

    add extension and move logic after evaluate

--- src/main/groovy/com/tmiyamon/MaterialDesignIconsPlugin.groovy
+++ src/main/groovy/com/tmiyamon/MaterialDesignIconsPlugin.groovy
@@ -8,53 +8,54 @@ import org.gradle.api.plugins.ApplicationPlugin
  * Created by tmiyamon on 12/24/14.
  */
 
-class MaterialDesignIconsPlugin implements Plugin<Project> {
+public class MaterialDesignIconsPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        project.task('fetchMaterialDesignIcons') {
-            def savedDir = new File("${System.getProperty("user.home")}/.material_design_icons")
-            def repoUrl = 'git@github.com:google/material-design-icons.git'
-            def resDirName = 'src/main/res'
+        project.extensions.create("mdicons", MaterialDesignIconsPluginExtension)
 
-            if (!savedDir.isDirectory()) {
-                def command = "git clone ${repoUrl} ${savedDir.getAbsolutePath()}"
+        project.afterEvaluate {
+            def repoUrl = 'git@github.com:google/material-design-icons.git'
+            def cacheDir = new File(project.mdicons.cachePath);
+
+            if (!cacheDir.isDirectory()) {
+                def command = "git clone ${repoUrl} ${cacheDir.getAbsolutePath()}"
                 command.execute().waitFor()
             }
-            if (savedDir.isDirectory()) {
+            if (cacheDir.isDirectory()) {
 
-                def resDir = new File(project.getProjectDir(), resDirName)
+                def resDir = new File(project.getProjectDir(), project.mdicons.resourceRelativePath)
 
                 if (!resDir.isDirectory()) {
                     resDir.mkdir();
                 }
 
-                savedDir.traverse {
-                    if (it.isFile() && it.getParentFile() != null &&
-                            it.getParentFile().getName().startsWith("drawable") &&
-                            it.getName().contains("search")
-                    ) {
-                        def iconFile = it
-                        def drawableTypeDir = iconFile.getParentFile().getName()
-                        def drawableDir = new File(resDir, drawableTypeDir)
-                        if (!drawableDir.isDirectory()) {
-                            drawableDir.mkdir()
-                        }
+                def pattern = project.mdicons.pattern
+                if (pattern != null) {
+                    cacheDir.traverse {
+                        if (it.isFile() && it.getParentFile() != null &&
+                                it.getParentFile().getName().startsWith("drawable") &&
+                                it.getName() =~ pattern
+                        ) {
+                            def iconFile = it
+                            def drawableTypeDir = iconFile.getParentFile().getName()
+                            def drawableDir = new File(resDir, drawableTypeDir)
+                            if (!drawableDir.isDirectory()) {
+                                drawableDir.mkdir()
+                            }
 
-                        if (!new File(drawableDir, iconFile.getName()).exists()) {
-                            project.logger.debug("copy from ${iconFile} to ${drawableDir}")
+                            if (!new File(drawableDir, iconFile.getName()).exists()) {
+                                project.logger.debug("copy from ${iconFile} to ${drawableDir}")
 
-                            project.copy {
-                                from iconFile
-                                into drawableDir
+                                project.copy {
+                                    from iconFile
+                                    into drawableDir
+                                }
                             }
                         }
                     }
                 }
             }
         }
-
-        project.plugins.withType(ApplicationPlugin) {
-            project.tasks.findByName('run').dependsOn('fetchMaterialDesignIcons')
-        }
     }
 }
+
---
+++ src/main/groovy/com/tmiyamon/MaterialDesignIconsPluginExtension.groovy
@@ -0,0 +1,12 @@
+package com.tmiyamon
+
+/**
+ * Created by tmiyamon on 12/24/14.
+ */
+public class MaterialDesignIconsPluginExtension {
+    String cachePath = "${System.getProperty("user.home")}/.material_design_icons"
+    String resourceRelativePath = 'src/main/res'
+    String pattern
+
+    public MaterialDesignIconsPluginExtension() {}
+}
