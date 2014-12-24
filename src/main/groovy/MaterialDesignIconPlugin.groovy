b8ca7371ae84770cf39e9edd6fde7cb54a42a6fe
tmiyamon <miyamototakuya@gmail.com>
2014-12-24 14:49:55 +0900

    add src

---
+++ src/main/groovy/MaterialDesignIconPlugin.groovy
@@ -0,0 +1,13 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by tmiyamon on 12/24/14.
+ */
+
+class MaterialDesignIconPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+
+    }
+}
