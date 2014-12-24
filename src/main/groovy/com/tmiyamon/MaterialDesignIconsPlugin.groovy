fb5cb2c689e0e555c632bcc9cd0a46924aee167c
tmiyamon <miyamototakuya@gmail.com>
2014-12-24 22:46:20 +0900

    add copy specific icons

--- src/main/groovy/com/tmiyamon/MaterialDesignIconsPlugin.groovy
+++ src/main/groovy/com/tmiyamon/MaterialDesignIconsPlugin.groovy
@@ -1,10 +1,8 @@
 package com.tmiyamon
 
-import com.android.builder.model.AndroidLibrary
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.ApplicationPlugin
-import org.gradle.api.plugins.JavaPlugin
 
 /**
  * Created by tmiyamon on 12/24/14.
@@ -14,11 +12,44 @@ class MaterialDesignIconsPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         project.task('fetchMaterialDesignIcons') {
-            def dest = "${System.getProperty("user.home")}/.material_design_icons"
+            def savedDir = new File("${System.getProperty("user.home")}/.material_design_icons")
+            def repoUrl = 'git@github.com:google/material-design-icons.git'
+            def resDirName = 'src/main/res'
 
-            if (!new File(dest).isDirectory()) {
-                def process = "git clone git@github.com:google/material-design-icons.git ${dest}".execute()
-                process.waitFor()
+            if (!savedDir.isDirectory()) {
+                def command = "git clone ${repoUrl} ${savedDir.getAbsolutePath()}"
+                command.execute().waitFor()
+            }
+            if (savedDir.isDirectory()) {
+
+                def resDir = new File(project.getProjectDir(), resDirName)
+
+                if (!resDir.isDirectory()) {
+                    resDir.mkdir();
+                }
+
+                savedDir.traverse {
+                    if (it.isFile() && it.getParentFile() != null &&
+                            it.getParentFile().getName().startsWith("drawable") &&
+                            it.getName().contains("search")
+                    ) {
+                        def iconFile = it
+                        def drawableTypeDir = iconFile.getParentFile().getName()
+                        def drawableDir = new File(resDir, drawableTypeDir)
+                        if (!drawableDir.isDirectory()) {
+                            drawableDir.mkdir()
+                        }
+
+                        if (!new File(drawableDir, iconFile.getName()).exists()) {
+                            project.logger.debug("copy from ${iconFile} to ${drawableDir}")
+
+                            project.copy {
+                                from iconFile
+                                into drawableDir
+                            }
+                        }
+                    }
+                }
             }
         }
 
