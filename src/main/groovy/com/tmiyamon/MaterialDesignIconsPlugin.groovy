714f65820d9b1898aa53299ea669068b2183e280
tmiyamon <miyamototakuya@gmail.com>
2014-12-25 01:32:07 +0900

    decouple to the tasks

--- src/main/groovy/com/tmiyamon/MaterialDesignIconsPlugin.groovy
+++ src/main/groovy/com/tmiyamon/MaterialDesignIconsPlugin.groovy
@@ -13,24 +13,31 @@ public class MaterialDesignIconsPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.extensions.create("mdicons", MaterialDesignIconsPluginExtension)
 
+        project.plugins.withType(ApplicationPlugin) {
+            project.tasks.findByName('run').dependsOn('copyIcons')
+        }
+
         project.afterEvaluate {
             def repoUrl = 'git@github.com:google/material-design-icons.git'
             def cacheDir = new File(project.mdicons.cachePath);
+            def pattern = project.mdicons.pattern
 
-            if (!cacheDir.isDirectory()) {
-                def command = "git clone ${repoUrl} ${cacheDir.getAbsolutePath()}"
-                command.execute().waitFor()
-            }
-            if (cacheDir.isDirectory()) {
-
-                def resDir = new File(project.getProjectDir(), project.mdicons.resourceRelativePath)
-
-                if (!resDir.isDirectory()) {
-                    resDir.mkdir();
+            project.task('cloneRepository') {
+                if (!cacheDir.isDirectory()) {
+                    def command = "git clone ${repoUrl} ${cacheDir.getAbsolutePath()}"
+                    command.execute().waitFor()
                 }
+            }
 
-                def pattern = project.mdicons.pattern
-                if (pattern != null) {
+            project.task('copyIcons', dependsOn: 'cloneRepository') {
+                if (cacheDir.isDirectory() && pattern != null) {
+
+                    def resDir = new File(project.getProjectDir(), project.mdicons.resourceRelativePath)
+
+                    if (!resDir.isDirectory()) {
+                        resDir.mkdir();
+                    }
+
                     cacheDir.traverse {
                         if (it.isFile() && it.getParentFile() != null &&
                                 it.getParentFile().getName().startsWith("drawable") &&
