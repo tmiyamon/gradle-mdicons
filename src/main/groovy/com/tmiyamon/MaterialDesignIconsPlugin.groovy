8352c01c8a83a159a038d2dee4c661a73cfbc34d
tmiyamon <miyamototakuya@gmail.com>
2014-12-25 02:53:25 +0900

    add clean task

--- src/main/groovy/com/tmiyamon/MaterialDesignIconsPlugin.groovy
+++ src/main/groovy/com/tmiyamon/MaterialDesignIconsPlugin.groovy
@@ -21,45 +21,73 @@ public class MaterialDesignIconsPlugin implements Plugin<Project> {
             def repoUrl = 'git@github.com:google/material-design-icons.git'
             def cacheDir = new File(project.mdicons.cachePath);
             def pattern = project.mdicons.pattern
+            def projectResourceDir = new File(project.getProjectDir(), project.mdicons.resourceRelativePath)
 
-            project.task('cloneRepository') {
+            def iconTypes = [
+                'action','alert','av','communication','content','device',
+                'editor','file','hardware','image','maps','navigation',
+                'notification','social','toggle'
+            ] as Set
+
+            def cloneRepository = project.task('cloneRepository') {
                 if (!cacheDir.isDirectory()) {
                     def command = "git clone ${repoUrl} ${cacheDir.getAbsolutePath()}"
                     command.execute().waitFor()
                 }
             }
 
-            project.task('copyIcons', dependsOn: 'cloneRepository') {
+            def cleanIcons = project.task('cleanIcons', dependsOn: 'cloneRepository') {
+                if (cacheDir.isDirectory()) {
+                    eachIconFiles(cacheDir, iconTypes, null) { cachedIconFile ->
+                        def projectTypedDrawableDir = new File(projectResourceDir, cachedIconFile.getParentFile().getName())
+
+                        def projectResourceFile = new File(projectTypedDrawableDir, cachedIconFile.getName())
+                        if (projectResourceFile.exists()) {
+                           projectResourceFile.delete()
+                        }
+                    }
+                }
+            }
+
+            def copyIconsDependencies = [cloneRepository]
+            if (project.mdicons.clean) {
+                copyIconsDependencies.add(cleanIcons)
+            }
+
+            project.task('copyIcons', dependsOn: copyIconsDependencies) {
                 if (cacheDir.isDirectory() && pattern != null) {
-
-                    def resDir = new File(project.getProjectDir(), project.mdicons.resourceRelativePath)
-
-                    if (!resDir.isDirectory()) {
-                        resDir.mkdir();
+                    if (!projectResourceDir.isDirectory()) {
+                        projectResourceDir.mkdir();
                     }
 
-                    cacheDir.traverse {
-                        if (it.isFile() && it.getParentFile() != null &&
-                                it.getParentFile().getName().startsWith("drawable") &&
-                                it.getName() =~ pattern
-                        ) {
-                            def iconFile = it
-                            def drawableTypeDir = iconFile.getParentFile().getName()
-                            def drawableDir = new File(resDir, drawableTypeDir)
-                            if (!drawableDir.isDirectory()) {
-                                drawableDir.mkdir()
-                            }
+                    eachIconFiles(cacheDir, iconTypes, pattern) { cachedIconFile ->
+                        def projectTypedDrawableDir = new File(projectResourceDir, cachedIconFile.getParentFile().getName())
+                        if (!projectTypedDrawableDir.isDirectory()) {
+                            projectTypedDrawableDir.mkdir()
+                        }
 
-                            if (!new File(drawableDir, iconFile.getName()).exists()) {
-                                project.logger.debug("copy from ${iconFile} to ${drawableDir}")
+                        if (!new File(projectTypedDrawableDir, cachedIconFile.getName()).exists()) {
+                            project.logger.debug("copy from ${cachedIconFile} to ${projectTypedDrawableDir}")
 
-                                project.copy {
-                                    from iconFile
-                                    into drawableDir
-                                }
+                            project.copy {
+                                from cachedIconFile
+                                into projectTypedDrawableDir
                             }
                         }
                     }
+                }
+            }
+
+        }
+    }
+
+    def eachIconFiles(File root, Set iconTypes, String pattern, Closure closure) {
+        root.eachDirMatch({ iconTypes.contains(new File(it).getName()) }) {
+            it.eachDirMatch({ new File(it).getName().startsWith("drawable")}) {
+                if (pattern != null) {
+                    it.eachFileMatch({ new File(it).getName() =~ pattern }, closure)
+                } else {
+                    it.eachFile(closure)
                 }
             }
         }
--- src/main/groovy/com/tmiyamon/MaterialDesignIconsPluginExtension.groovy
+++ src/main/groovy/com/tmiyamon/MaterialDesignIconsPluginExtension.groovy
@@ -7,6 +7,7 @@ public class MaterialDesignIconsPluginExtension {
     String cachePath = "${System.getProperty("user.home")}/.material_design_icons"
     String resourceRelativePath = 'src/main/res'
     String pattern
+    boolean clean
 
     public MaterialDesignIconsPluginExtension() {}
 }
